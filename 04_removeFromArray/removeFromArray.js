3aabe986bb10b0a82a7d6f2ed284f8328aac4d59
Echo <echofox359@gmail.com>
2024-07-29 22:29:27 -0400

    Complete Module 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(startingArray, ...args) {
+    let newArray = []
+    startingArray.forEach(element => {
+        let flag = false;
+        for (let i = 0; i < args.length; i++) {
+            if(args[i] === element){
+                flag = true;
+            }
+        }
+        if(!flag){
+            newArray.push(element)
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
