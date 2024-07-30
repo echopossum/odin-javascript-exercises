6820aeffe875cd49def6f56db5b10259479b879d
Echo <echofox359@gmail.com>
2024-07-29 21:41:51 -0400

    Complete module 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let newStrArray = str.split("")
+    return newStrArray.reverse().join("")
 };
 
 // Do not edit below this line
