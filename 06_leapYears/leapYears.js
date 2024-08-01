4d95dcdc592facb9809fba7b596370d2a9b98c7d
Echo <echofox359@gmail.com>
2024-07-31 21:03:39 -0400

    Complete module 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let isDivisibleByFour = year % 4 == 0
+    let isDivisibleByOnehundred = year % 100 == 0
+    let isDivisibleByFourHundred = year % 400 == 0
+    
+    
+    if(isDivisibleByFour && (!isDivisibleByOnehundred || isDivisibleByFourHundred)){
+        return true
+    }
+    else{
+        return false
+    }
 
+    
 };
 
 // Do not edit below this line
