468e2eac1ac56bb0ff5506c144aea8b6498dccd9
Echo <echofox359@gmail.com>
2024-07-31 21:15:16 -0400

    Complete module 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let conversion =  5/9*(f - 32)
+  return Math.round(conversion*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let conversion = c*1.8+32
+  return Math.round(conversion * 10)/10
 };
 
 // Do not edit below this line
