2ba84e8a08b5f22f923b7954270705d541b4b6ff
Echo <echofox359@gmail.com>
2024-07-31 20:44:31 -0400

    Complete module 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(starting,ending) {
+    if(starting <= 0 || ending <= 0) return 'ERROR'
+    if(!Number.isInteger(starting) || !Number.isInteger(ending)) return 'ERROR'
 
+    let min = (starting > ending) ? ending : starting
+    let max = (starting > ending) ? starting : ending
+
+    let sum = 0
+
+    for(let i= min;i <= max; i++){
+        sum +=i
+    }
+
+    return sum
 };
 
 // Do not edit below this line
